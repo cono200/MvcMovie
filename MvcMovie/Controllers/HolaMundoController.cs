dd8e1e3f5d957b5c01dc310fe40b59cbcf2d4f97
cono200 <tatattian04@gmail.com>
2023-07-06 13:19:45 -0700

    Agregar archivos de proyecto.

---
+++ MvcMovie/Controllers/HolaMundoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MvcMovie.Controllers
+{
+    public class HolaMundoController : Controller
+    {
+        public IActionResult  Index()
+        {
+            return View();
+        }
+        public IActionResult Bievenido(string nombre, int NumVeces=1)
+        {
+            ViewData["Mensaje"] = "Hola " + nombre;
+            ViewData["NumVeces"] = NumVeces;
+            return View();
+         }
+
+        public  int Suma(int numero1,int numero2,int resultado)
+        {
+            resultado = numero1 + numero2;
+            return (resultado);
+        }
+    }
+}
